7d1ae8777c8a23406d4b32811202a743c8af028b
Dylan Hrebenach <dhrebenach@underdogmedia.com>
2020-09-02 11:49:09 -0700

    Finished DFS on isBalanced function.

--- isSuperBalanced.js
+++ isSuperBalanced.js
@@ -19,7 +19,39 @@ class BinaryTreeNode {
 function isBalanced(treeRoot) {
 
   // Determine if the tree is superbalanced
-  
 
-  return false;
+  // tree with no nodes is superbalanced
+  if (!treeRoot) {
+    return true
+  }
+
+  let leafDepths = []
+  let nodes = []
+
+  nodes.push([treeRoot, 0])
+
+  while (nodes.length) {
+    const nodePair = nodes.pop()
+    const node = nodePair[0]
+    const depth = nodePair[1]
+
+    // this node is a leaf
+    if (!node.left && !node.right) {
+      if (!leafDepths.includes(depth)) {
+        leafDepths.push(depth)
+        if (leafDepths.length > 2 || (leafDepths.length === 2 && Math.abs(leafDepths[0] - leafDepths[1]) > 1)) {
+          return false
+        }
+      }
+    } else {
+      if (node.left) {
+        nodes.push([node.left, depth + 1])
+      }
+      if (node.right) {
+        nodes.push([node.right, depth + 1])
+      }
+    }
+  }
+
+  return true
 }
